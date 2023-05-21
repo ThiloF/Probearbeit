5a11a8e90832a1d84e2a6ae0cd7027ea92895d5c
Thilo Falkenstein <thilo-falkenstein@unitybox.de>
2023-05-21 08:27:48 +0200

    fixed: Taksupdate

--- src/buttons/statusButton/StatusButton.tsx
+++ src/buttons/statusButton/StatusButton.tsx
@@ -2,15 +2,19 @@ import Styling from "./StatusButton.module.css";
 
 type Props = {
   status: boolean;
+  taskname: string;
   handleStatus: (is_completed: boolean) => void;
+  handleUpdate: (title:string, status:boolean) => void;
 };
 
-export default function StatusButton({ status, handleStatus }: Props) {
+export default function StatusButton({ status, taskname, handleStatus, handleUpdate }: Props) {
   function switchStatus() {
     if (status === true) {
       handleStatus(false);
+      handleUpdate(taskname, false)
     } else {
       handleStatus(true);
+      handleUpdate(taskname, true)
     }
   }
 
